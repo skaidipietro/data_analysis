596bfa08af644ea9f5589b08daa59df3b57ac9b2
Skai Dipietro <skaidipietro@stu.xhu.edu.cn>
2025-06-11 15:57:32 +1000

    this is my first assignment

---
+++ studioR1.R
@@ -0,0 +1,9 @@
+#########################################################
+# R script: studio1.R
+# Project: Studio 1
+#
+# Date: 19/07/2017
+# Author: Chenlongjie Weng
+#
+# Purpose: Introductory script for Studio 1
+#########################################################
